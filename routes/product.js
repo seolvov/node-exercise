740211492a95976a71d8b1cf0eca3b41f2a27dff
seolvov <hhhseolah@gmail.com>
2023-05-13 11:05:20 +0900

    product get 전체데이터 > get 상세데이터

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,37 @@
 import express from "express";
 import productModel from "../models/product.js";
+import product from "../models/product.js";
 
 const router = express.Router()
 
-router.get("/all", (req, res) => {
+router.get("/all", async (req, res) => {
+    const products = await productModel.find() //find() 는 array 로 return
     res.json({
-        msg: "product get all"
+        msg: "product get all",
+        count: products.length,
+        // products: products,
+        products: products.map(item => { //해당 컬럼만 가져오기
+            return {
+                id: item._id,
+                title: item.title,
+                price: item.price
+            }
+        })
     })
+})
+//상세 데이터 가져오는 API
+router.get("/:productId", async(req, res) => {
+    const product = await productModel.findById(req.params.productId) //url 들어가는 params
+    // if (!product) {
+    //     res.json({
+    //         msg: "no product"
+    //     })
+    // } else {
+        res.json({
+            msg: `successful get ${req.params.productId}`,
+            product: product
+        })
+    // }
 })
 router.post("/", async (req, res) => {
     // const userInput = {
