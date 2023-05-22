d6e2c8c6bb8cafa88df1b42791c327cb2ab8ab32
seolvov <hhhseolah@gmail.com>
2023-05-22 20:34:57 +0900

    create userModel

--- models/user.js
+++ models/user.js
@@ -1,7 +1,30 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
-    {},
+    // 패스워드, 이름, 주소, 이메일, 휴대전화, 비고
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: Number,
+            required: true
+        }
+    },
     {
         timestamps: true
     }
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,29 @@ import userModel from "../models/user.js";
 
 const router = express.Router()
 
+//회원가입
+router.post("/signup", async (req, res) => {
+    const { email, password, userName, address, phone } = req.body
+    const newUser = new userModel({
+        email, password, userName, address, phone
+    })
+    const createdUser = await newUser.save()
+    res.json({
+        msg: "created ID",
+        user: createdUser
+    })
+})
+//로그인
+router.post("/login", async (req, res) => {
+    res.json({
+        msg: "success"
+    })
+})
+//정보가져오기
+router.get("/", async (req, res) => {
+    res.json( {
+        msg: "get all"
+    })
+})
+
 export default router
