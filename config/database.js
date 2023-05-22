d0686a2944f2517491cf87848ba914f59e5acc39
seolvov <hhhseolah@gmail.com>
2023-05-22 19:16:41 +0900

    connect docker

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_DB)
         console.log("connected database")
     } catch(err) {
         console.log(err.message)
